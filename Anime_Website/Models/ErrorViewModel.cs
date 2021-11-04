20782e9ba62fdbee6e9e61bec924abfdeaf31a6b
Hattinger04 <s.8greiderer@gmail.com>
2021-11-04 08:32:59 +0100

    initialize

---
+++ Anime_Website/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Anime_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
