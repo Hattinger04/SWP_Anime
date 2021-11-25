863c2a14803544c86a2f38764326de01961f732e
Julius Greil <jugreil@tsn.at>
2021-11-25 09:38:13 +0100

    Nach Untericht

--- Anime_Website/wwwroot/js/site.js
+++ Anime_Website/wwwroot/js/site.js
@@ -1,1 +1,22 @@
-﻿
+﻿function displayNextImage() {
+    x = (x === images.length - 1) ? 0 : x + 1;
+document.getElementById("img").src = images[x];
+}
+
+function startTimer() {
+    setInterval(displayNextImage, 10000);
+}
+
+function fullFett() {
+    var el = document.documentElement,
+        rfs = el.requestFullScreen
+            || el.webkitRequestFullScreen
+            || el.mozRequestFullScreen;
+    rfs.call(el);
+};
+
+var images = [];
+x = -1;
+images[0] = "../images/1.jpg";
+images[1] = "../images/2.jpg";
+
