9d7ff12fda76d0f94ba2e6642876362bd53655bf
Julius Greil <jugreil@tsn.at>
2021-11-25 12:52:20 +0100

    alllllessss

--- Anime_Website/wwwroot/js/site.js
+++ Anime_Website/wwwroot/js/site.js
@@ -1,5 +1,6 @@
 ﻿function displayNextImage() {
-    x = (x === images.length - 1) ? 0 : x + 1;
+    x = x + 1;
+    x = x % 4;
 document.getElementById("img").src = images[x];
 }
 
@@ -7,16 +8,22 @@ function startTimer() {
     setInterval(displayNextImage, 10000);
 }
 
-function fullFett() {
-    var el = document.documentElement,
-        rfs = el.requestFullScreen
-            || el.webkitRequestFullScreen
-            || el.mozRequestFullScreen;
-    rfs.call(el);
-};
+function enterFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.msRequestFullscreen) {      // for IE11 (remove June 15, 2022)
+        element.msRequestFullscreen();
+    } else if (element.webkitRequestFullscreen) {  // iOS Safari
+        element.webkitRequestFullscreen();
+    }
+}
+
+
 
 var images = [];
-x = -1;
+x = 0;
 images[0] = "../images/1.jpg";
 images[1] = "../images/2.jpg";
+images[2] = "../images/3.jpg";
+images[3] = "../images/4.jpg";
 
