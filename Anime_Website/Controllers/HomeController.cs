21a07e91d0561ac0f85b07c85f44b6a957aedd35
Hattinger04 <s.8greiderer@gmail.com>
2022-04-22 20:29:01 +0200

    adding api

--- Anime_Website/Controllers/HomeController.cs
+++ Anime_Website/Controllers/HomeController.cs
@@ -20,12 +20,31 @@ namespace Anime_Website.Controllers {
 
         [HttpPost]
         public async Task<IActionResult> Index(string name) {
+            name = name.Replace(" ", "%20"); 
             string message = "";
             HttpResponseMessage response = await client.GetAsync(String.Format("https://api.jikan.moe/v4/anime?q={0}&sfw", name));
             if (response.IsSuccessStatusCode) {
-                // bearbeiten des json vll
                 message = await response.Content.ReadAsStringAsync();
-                return View(message);
+                dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(message);
+                List<AnimeObject> animeObjects = new List<AnimeObject>();
+                string countString = jsonObj["pagination"]["items"]["count"];
+                int count = Int32.Parse(countString); 
+                if (count != 0) { 
+                    for (int i = 0; i < count; i++) {
+                        animeObjects.Add(new AnimeObject() {
+                            ID = i,
+                            Image_URL = jsonObj["data"][i]["images"]["jpg"]["image_url"],
+                            Name = jsonObj["data"][i]["title"],
+                            Status = jsonObj["data"][i]["status"],
+                            URL = jsonObj["data"][i]["url"],
+                        });
+                    }
+                    foreach (AnimeObject obj in animeObjects) {
+                        Console.WriteLine(obj.Name);
+                    }
+                    return View("Index", animeObjects);
+                }
+                return View("_Message", new Message("Kein Ergebnis!", "Die Abfrage hat kein Ergebnis geliefert!"));
             }
             return View("_Message", new Message("HTTPClient Error!", "Die Abfrage hat nicht funktioniert, versuche es bitte später erneut!")); 
         }
---
+++ Anime_Website/Models/AnimeObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Anime_Website.Models {
+    public class AnimeObject {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string URL { get; set; }
+        public string Image_URL { get; set; }
+        public string Status { get; set; }
+    }
+}
