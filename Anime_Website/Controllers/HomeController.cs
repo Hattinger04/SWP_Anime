54bd7f9b9ea29bfabc4866b614d5041854215e64
Hattinger04 <s.8greiderer@gmail.com>
2022-04-26 19:25:49 +0200

    rss

--- Anime_Website/Controllers/HomeController.cs
+++ Anime_Website/Controllers/HomeController.cs
@@ -7,6 +7,11 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Net.Http;
+using System.ServiceModel;
+using System.Xml;
+using System.ServiceModel.Syndication;
+using Anime_Website.Models;
+
 
 namespace Anime_Website.Controllers {
     public class HomeController : Controller {
@@ -51,8 +56,31 @@ namespace Anime_Website.Controllers {
         public IActionResult AboutUs() {
             return View();
         }
+        
         public IActionResult Discord() {
-            return View();
+            return View(); 
+        }
+
+        public IActionResult Schedule() {
+            string url = "https://www.livechart.me/feeds/episodes";
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+            List<AnimeObject> animeObjects = new();
+            int counter = 0;
+            foreach (var item in feed.Items) {
+                foreach (SyndicationElementExtension extension in item.ElementExtensions) {
+                    animeObjects.Add(new AnimeObject() {
+                        ID = counter,
+                        Name = item.Title.Text,
+                        Status = "running",
+                        Image_URL = item.ElementExtensions.FirstOrDefault(ext => ext.OuterName == "thumbnail").GetObject<XmlElement>().Attributes.GetNamedItem("url").Value,
+                        URL = item.Id
+                    });
+                    counter++;
+                }
+            }
+            return View("Schedule", animeObjects);
         }
     }
 }
